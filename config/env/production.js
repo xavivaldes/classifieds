4209ad5184e65d6268fe5ee4599927a2855248b3
Xavi Valdes <xavivaldes@gmail.com>
2015-04-21 21:33:54 +0200

    merge

--- config/env/production.js
+++ config/env/production.js
@@ -25,11 +25,14 @@ module.exports = {
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
+				'public/lib/angular-i18n/angular-locale_es-es.js',
 				'public/lib/angular-resource/angular-resource.min.js',
 				'public/lib/angular-animate/angular-animate.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/ng-file-upload/angular-file-upload.min.js',
+				'public/lib/ng-file-upload-shim/angular-file-upload-shim.min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
