dee9a8a3675f9831912720bf844c2577800e530a
Xavi Valdes <xavivaldes@gmail.com>
2015-05-02 10:51:59 +0200

    grunt build

--- public/dist/application.js
+++ public/dist/application.js
@@ -350,147 +350,110 @@ angular.module('classifieds').config(['$stateProvider',
 
 // Classifieds controller
 angular.module('classifieds').controller('ClassifiedsController', ['$scope', '$upload', '$stateParams', '$location', 'Authentication', 'Classifieds', 'Categories', 'Families', 'InstrumentTypes',
-	function ($scope, $upload, $stateParams, $location, Authentication, Classifieds, Categories, Families, InstrumentTypes) {
-		$scope.authentication = Authentication;
+    function ($scope, $upload, $stateParams, $location, Authentication, Classifieds, Categories, Families, InstrumentTypes) {
+        $scope.authentication = Authentication;
 
-		$scope.sendFile = function (classifiedId) {
-			var file = $scope.files[0];
-			console.log(file);
-			$scope.upload = $upload.upload({
-				url: 'classifieds/pic', //upload.php script, node.js route, or servlet url
-				//method: 'POST' or 'PUT',
-				//headers: {'header-key': 'header-value'},
-				//withCredentials: true,
-				data: {classified: classifiedId},
-				file: file // or list of files ($files) for html5 only
-				//fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-				// customize file formData name ('Content-Desposition'), server side file variable name.
-				//fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file'
-				// customize how data is added to formData. See #40#issuecomment-28612000 for sample code
-				//formDataAppender: function(formData, key, val){}
-			}).progress(function (evt) {
-				console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-			}).success(function (data, status, headers, config) {
-				// file is uploaded successfully
-				console.log(data);
-			});
-		};
+        $scope.sendClassified = function (classified, callback) {
+            var file = $scope.files[0];
+            console.log(file);
+            $scope.upload = $upload.upload({
+                url: 'classifieds',
+                data: {classified: classified},
+                file: file // or list of files ($files) for html5 only
+            }).success(function (data) {
+                console.log(data);
+                callback(data);
+            });
+        };
 
-		$scope.sendClassified = function (classified) {
-			var file = $scope.files[0];
-			console.log(file);
-			$scope.upload = $upload.upload({
-				url: 'classifieds', //upload.php script, node.js route, or servlet url
-				//method: 'POST' or 'PUT',
-				//headers: {'header-key': 'header-value'},
-				//withCredentials: true,
-				data: {classified: classified},
-				file: file // or list of files ($files) for html5 only
-				//fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-				// customize file formData name ('Content-Desposition'), server side file variable name.
-				//fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file'
-				// customize how data is added to formData. See #40#issuecomment-28612000 for sample code
-				//formDataAppender: function(formData, key, val){}
-			}).progress(function (evt) {
-				console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-			}).success(function (data, status, headers, config) {
-				// file is uploaded successfully
-				console.log(data);
-			});
-		};
+        // Create new Classified
+        $scope.create = function () {
+            // Create new Classified object
+            var classified = new Classifieds({
+                shortDescription: this.shortDescription,
+                longDescription: this.longDescription,
+                price: this.price,
+                category: this.category,
+                family: this.family,
+                instrumentType: this.instrumentType,
+                pics: this.pics,
+                files: this.files
+            });
 
-		// Create new Classified
-		$scope.create = function () {
-			// Create new Classified object
-			var classified = new Classifieds({
-				shortDescription: this.shortDescription,
-				longDescription: this.longDescription,
-				price: this.price,
-				category: this.category,
-				family: this.family,
-				instrumentType: this.instrumentType,
-				pics: this.pics,
-				files: this.files
-			});
+            $scope.sendClassified(classified, function (response) {
 
-			$scope.sendClassified(classified);
+                $location.path('classifieds/' + response._id);
 
-			// Redirect after save
-			/*
-			classified.$save(function (response) {
+                // Clear form fields
+                $scope.name = '';
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-				$scope.sendFile(response._id);
+        // Remove existing Classified
+        $scope.remove = function (classified) {
+            if (classified) {
+                classified.$remove();
 
-				$location.path('classifieds/' + response._id);
+                for (var i in $scope.classifieds) {
+                    if ($scope.classifieds [i] === classified) {
+                        $scope.classifieds.splice(i, 1);
+                    }
+                }
+            } else {
+                $scope.classified.$remove(function () {
+                    $location.path('classifieds');
+                });
+            }
+        };
 
-				// Clear form fields
-				$scope.name = '';
-			}, function (errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-			*/
-		};
+        // Update existing Classified
+        $scope.update = function () {
+            var classified = $scope.classified;
 
-		// Remove existing Classified
-		$scope.remove = function (classified) {
-			if (classified) {
-				classified.$remove();
+            classified.$update(function () {
+                $location.path('classifieds/' + classified._id);
+            }, function (errorResponse) {
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
-				for (var i in $scope.classifieds) {
-					if ($scope.classifieds [i] === classified) {
-						$scope.classifieds.splice(i, 1);
-					}
-				}
-			} else {
-				$scope.classified.$remove(function () {
-					$location.path('classifieds');
-				});
-			}
-		};
+        // Find a list of Classifieds
+        $scope.find = function () {
+            $scope.classifieds = Classifieds.query();
+        };
 
-		// Update existing Classified
-		$scope.update = function () {
-			var classified = $scope.classified;
+        $scope.loadAdditionalInfo = function () {
+            $scope.categories = Categories.query();
+            $scope.families = Families.query();
+        };
 
-			classified.$update(function () {
-				$location.path('classifieds/' + classified._id);
-			}, function (errorResponse) {
-				$scope.error = errorResponse.data.message;
-			});
-		};
+        // Find existing Classified
+        $scope.findOne = function () {
+            $scope.classified = Classifieds.get({
+                classifiedId: $stateParams.classifiedId
+            });
+            //console.log($scope.classified); <-- es una promise...
+            $scope.image = 'data:' + $scope.classified.pic.contentType + ';base64,' + $scope.classified.pic.data;
+        };
 
-		// Find a list of Classifieds
-		$scope.find = function () {
-			$scope.classifieds = Classifieds.query();
-		};
+        $scope.$watch('family', function (newVal) {
+            if (newVal) $scope.instrumentTypes = InstrumentTypes.query({family: newVal});
+        });
 
-		$scope.loadAdditionalInfo = function () {
-			$scope.categories = Categories.query();
-			$scope.families = Families.query();
-		};
+        $scope.$watch('files', function () {
+            if ($scope.files && $scope.files.length > 0) {
+                var fileReader = new FileReader();
 
-		// Find existing Classified
-		$scope.findOne = function () {
-			$scope.classified = Classifieds.get({
-				classifiedId: $stateParams.classifiedId
-			});
-		};
-
-		$scope.$watch('family', function (newVal) {
-			if (newVal) $scope.instrumentTypes = InstrumentTypes.query({family: newVal});
-		});
-
-		$scope.$watch('pics', function (newVal) {
-			console.log('pics changed: ' + newVal ? newVal[0] : 'nothing');
-		});
-
-		$scope.$watch('files', function (newVal) {
-			var files = $scope.files;
-			//for (var i = 0; i < files.length; i++) {
-				//$scope.sendFile();
-			//}
-		});
-	}
+                fileReader.onload = function (fileLoadedEvent) {
+                    console.log(fileLoadedEvent.target.result);
+                    document.getElementById('image-preview').innerHTML = '<img src="' + fileLoadedEvent.target.result + '"/>';
+                };
+                fileReader.readAsDataURL($scope.files[0]);
+            }
+        });
+    }
 ]);
 
 'use strict';
